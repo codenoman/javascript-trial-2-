d1456575b32b17d854c933ab968107108032b213
codenoman <khannomanmohd123@gmail.com>
2025-11-25 14:38:35 +0000

    inital mes

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("hello woeld")
+// let a = "noman  " 
+// console.log(a+" world ")
+console.log("Hello world");
+
+const accountId= 144553
+let accountEmail ="helloworld@gmail.com"
+var accountPassword = "12345"
+let accountCity ="jaipur"
+
+
+
+console.log(accountId);
