eceab68b7818a89630a9e3e21e9ffccacb1bb105
codenoman <khannomanmohd123@gmail.com>
2025-11-25 15:13:55 +0000

    variable

--- 01_script.js
+++ 01_script.js
@@ -6,8 +6,13 @@ console.log("Hello world");
 const accountId= 144553
 let accountEmail ="helloworld@gmail.com"
 var accountPassword = "12345"
-let accountCity ="jaipur"
+//let accountCity ="jaipur"
 
-
+accountCity= "Jaipur"
+let accountState=""
+/*prefer not use  var
+because of issue in block scope and functional scope 
+*/
 
 console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
