f190f7d1fa888e699efeac9dba17dda463a6725e
codenoman <khannomanmohd123@gmail.com>
2025-11-26 17:17:58 +0000

    conversionopertion

---
+++ conversion.js
@@ -0,0 +1,23 @@
+let score= undefined   //"33abv"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInnumber= Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//"33"=> 33
+//"33abv"=> NaN
+// true=> 1; false=>0
+// undefined=> NaN
+
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+// "hitesh"=> true
+let somenumber=33
+let stringNumber=String(somenumber)
---
+++ datatype.js
@@ -0,0 +1,22 @@
+"use strict "// treat all JS code as newer version
+
+// alert(3 +3) // we are using node.js,not browser 
+
+console.log(3+3); //code readability should be high 
+console.log("hello");
+
+let name="mohd"
+let age=18
+let isloggedin= false 
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//undefined =>
+//symbol => unique
+
+//object
+
+//console.log(type of undefined);// undefined 
+//console.log(type of null);//object
