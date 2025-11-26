70d279189ff333f8aecfb1d4995be8a6800ff99c
codenoman <khannomanmohd123@gmail.com>
2025-11-26 17:34:27 +0000

    conversionoperation

---
+++ operation.js
@@ -0,0 +1,26 @@
+let value =3
+let negvalue =-value
+
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(4/2);
+console.log(5%2); //gives remainder
+console.log(2**3); //2 power 3=8
+
+let str1="hello"
+let str2="mohd"
+console.log(str1+" "+str2);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+console.log(+true);
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
