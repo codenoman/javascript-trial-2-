fae480ab3031c2e90b9e33b2e3b07beb2fc7b36d
codenoman <khannomanmohd123@gmail.com>
2025-11-26 17:47:56 +0000

    comparision

---
+++ comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 2);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 = 2);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" >1) //string with number
+
+console.log(null >0); //use to avoid these conversion
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <0);
+
+//===
+console.log("2" === 2);
