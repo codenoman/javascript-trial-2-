1db5db5b4156caa78b4a47f797476091d4c09ba6
codenoman <khannomanmohd123@gmail.com>
2025-11-27 19:12:44 +0000

    datatypesummary

---
+++ datasum.js
@@ -0,0 +1,32 @@
+// Primitive
+//7 types: String,number,boolean,null,undefined,symbol,big int 
+
+const score=100
+const scoreValue =100.3
+
+const isLoggedIn= false
+const outsideTemp= null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+const bigNumber=326543576654356754n 
+
+
+//Reference(Non primitive)
+
+//Array,Objects,functions
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj = {
+    name:"mohd",
+    age:22,
+}
+
+const myFunction=function(){
+ console.log("helloworld");  
+ 
+}
+
+console.log(typeof anotherId);
