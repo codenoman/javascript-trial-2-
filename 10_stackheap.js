5ce56a8e5288bc1dbc7a8d6dc89cf3286cc1a425
codenoman <khannomanmohd123@gmail.com>
2025-12-03 17:34:04 +0000

    stack&heap

---
+++ 10_stackheap.js
@@ -0,0 +1,24 @@
+// Stack(primitive) Heap(Non-Primitive)
+
+// const { useInsertionEffect } = require("react");
+
+// let mycollegename ="Recazamgarh"
+
+// let anothername=mycollegename
+// anothername="chaiandcode"
+
+// console.log(mycollegename);
+// console.log(anothername);
+
+let userOne={
+    email:"user@gmail.com",
+    upi :"user@ybl"
+}
+
+let usertwo=userOne
+
+usertwo.email="hello@gmail.com"
+
+console.log(userOne.email);
+console.log(userOne.email);
+
